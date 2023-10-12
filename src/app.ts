a4bd262ad7a24eb4e2de7df35805da91fbfbd772
fzl96 <fpsdragon333@gmail.com>
2023-10-12 09:08:05 +0700

    fix: sends 404 when the route is not found

--- src/app.ts
+++ src/app.ts
@@ -47,5 +47,8 @@ app.use("/purchases", authenticateToken, purchasesRoute);
 app.use("/suppliers", authenticateToken, suppliersRoute);
 app.use("/cashier", authenticateToken, cashierRoute);
 app.use("/groups", authenticateToken, groupsRoute);
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Not found" });
+});
 
 export default app;
