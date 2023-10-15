c1f3bb45e15c391cb78be556926c8e469700c5b3
fzl96 <fpsdragon333@gmail.com>
2023-10-15 13:28:53 +0700

    feat: add pagination utility function

---
+++ src/utils/pagination.ts
@@ -0,0 +1,28 @@
+type Pagination = {
+  page: number;
+  pageSize: number;
+  total: number;
+  url: string;
+}
+
+export function createPagination ({
+  page,
+  pageSize,
+  total,
+  url,
+}: Pagination) {
+  const pagination = {
+    page: page,
+    pageSize: page ? pageSize : total,
+    total: total,
+    totalPages: page ? Math.ceil(total / pageSize) : 1,
+    previous: page > 1 ? `${url}?page=${page - 1}&pageSize=${pageSize}` : null,
+    next: page < Math.ceil(total / pageSize) ? `${url}?page=${page + 1}&pageSize=${pageSize}` : null,
+    links: {
+      first: page ? `${url}?page=1&pageSize=${pageSize}` : null,
+      last: page ? `${url}?page=${Math.ceil(total / pageSize)}&pageSize=${pageSize}` : null,
+    }
+  }
+
+  return pagination;
+}
