14da5988f91fc65da54520567a272095bceed6b0
fzl96 <fpsdragon333@gmail.com>
2023-10-16 12:11:07 +0700

    feat: add swagger

--- src/routes/categories-route.ts
+++ src/routes/categories-route.ts
@@ -10,7 +10,46 @@ import {
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /categories:
+ *  get:
+ *    description: Get all categories
+ *    tags:
+ *      - Categories
+ *    responses:
+ *      200:
+ *        description: OK
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                type: object
+ *                properties:
+ *                  id:
+ *                    type: string
+ *                  name:
+ *                    type: string
+ *                  createdAt:
+ *                    type: string
+ *                  updatedAt:
+ *                    type: string
+ *            example:
+ *              - id: "1"
+ *                name: "Category 1"
+ *                createdAt: "2022-01-01T00:00:00.000Z"
+ *                updatedAt: "2022-01-01T00:00:00.000Z"
+ *              - id: "2"
+ *                name: "Category 2"
+ *                createdAt: "2022-01-02T00:00:00.000Z"
+ *                updatedAt: "2022-01-02T00:00:00.000Z"
+ *      500:
+ *        description: Internal Server Error
+ */
 router.get("/", getCategories);
+
+
 router.get("/:id", getCategory);
 router.post("/", createCategory);
 router.put("/:id", updateCategory);
--- src/routes/products-route.ts
+++ src/routes/products-route.ts
@@ -10,11 +10,164 @@ import {
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Products
+ *   description: API for managing products
+ */
+
+/**
+ * @swagger
+ * /products:
+ *   get:
+ *     summary: Get all products
+ *     tags: [Products]
+ *     responses:
+ *       200:
+ *         description: OK
+ *       500:
+ *         description: Internal Server Error
+ */
 router.get("/", getProducts);
+
+/**
+ * @swagger
+ * /products/{id}:
+ *   get:
+ *     summary: Get a product by ID
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the product to get
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: OK
+ *       404:
+ *         description: Product not found
+ *       500:
+ *         description: Internal Server Error
+ */
 router.get("/:id", getProduct);
+
+/**
+ * @swagger
+ * /products:
+ *   post:
+ *     summary: Create a new product
+ *     tags: [Products]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               price:
+ *                 type: number
+ *               categoryId:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Created
+ *       400:
+ *         description: Bad Request
+ *       500:
+ *         description: Internal Server Error
+ */
 router.post("/", createProduct);
+
+/**
+ * @swagger
+ * /products/{id}:
+ *   put:
+ *     summary: Update a product by ID
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the product to update
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               price:
+ *                 type: number
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Bad Request
+ *       404:
+ *         description: Product not found
+ *       500:
+ *         description: Internal Server Error
+ */
 router.put("/:id", updateProduct);
+
+/**
+ * @swagger
+ * /products/{id}:
+ *   delete:
+ *     summary: Delete a product by ID
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the product to delete
+ *         schema:
+ *           type: string
+ *     responses:
+ *       204:
+ *         description: No Content
+ *       404:
+ *         description: Product not found
+ *       500:
+ *         description: Internal Server Error
+ */
 router.delete("/:id", deleteProduct);
+
+/**
+ * @swagger
+ * /products:
+ *   delete:
+ *     summary: Delete multiple products
+ *     tags: [Products]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: array
+ *             items:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: string
+ *                   description: ID of the product to delete
+ *     responses:
+ *       204:
+ *         description: No Content
+ *       400:
+ *         description: Bad Request
+ *       500:
+ *         description: Internal Server Error
+ */
 router.delete("/", deleteProducts);
 
 export default router;
