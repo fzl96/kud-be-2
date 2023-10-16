04e4b98afda00c84647b4a6f02aea26be8a56218
fzl96 <fpsdragon333@gmail.com>
2023-10-16 09:29:55 +0700

    feat: add role authorization

--- src/app.ts
+++ src/app.ts
@@ -16,16 +16,21 @@ import { getSalesData } from "./controllers/dashboard-controller.js";
 import { authenticateToken } from "./middleware/auth-middleware.js";
 import { exportProducts } from "./controllers/products-controller.js";
 import dotenv from "dotenv";
+import morgan from "morgan";
+import swaggerUI from "swagger-ui-express";
+import { specs } from "./utils/swagger.js";
+import { authorize } from "./middleware/permissions-middleware.js";
 
 const app = express();
 dotenv.config();
 
 app.use(cookieParser());
 app.use(express.json());
+app.use(morgan("dev"));
 app.use(
   cors({
     credentials: true,
-    origin: ["https://kud-henna.vercel.app", "http://localhost:5173"],
+    origin: ["http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
@@ -35,18 +40,19 @@ app.get("/", (req, res) => {
   res.json({ message: "hello world" });
 });
 app.use("/auth", authRoute);
-app.get("/dashboard/:year", authenticateToken, getSalesData);
-app.use("/categories", authenticateToken, categoriesRoute);
+app.get("/dashboard/:year", authenticateToken, authorize, getSalesData);
+app.use("/categories", authenticateToken, authorize, categoriesRoute);
 app.use("/products", authenticateToken, productsRoute);
-app.use("/export/products", authenticateToken, exportProducts);
-app.use("/users", authenticateToken, usersRoute);
-app.use("/members", authenticateToken, membersRoute);
-app.use("/sales", authenticateToken, salesRoute);
-app.use("/credit-payment", authenticateToken, creditPaymentRoute);
-app.use("/purchases", authenticateToken, purchasesRoute);
-app.use("/suppliers", authenticateToken, suppliersRoute);
-app.use("/cashier", authenticateToken, cashierRoute);
-app.use("/groups", authenticateToken, groupsRoute);
+app.use("/export/products", authenticateToken, authorize, exportProducts);
+app.use("/users", authenticateToken, authorize, usersRoute);
+app.use("/members", authenticateToken, authorize, membersRoute);
+app.use("/sales", authenticateToken, authorize, salesRoute);
+app.use("/credit-payment", authenticateToken, authorize, creditPaymentRoute);
+app.use("/purchases", authenticateToken, authorize, purchasesRoute);
+app.use("/suppliers", authenticateToken, authorize, suppliersRoute);
+app.use("/cashier", authenticateToken, authorize, cashierRoute);
+app.use("/groups", authenticateToken, authorize, groupsRoute);
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(specs));
 app.use((req, res, next) => {
   res.status(404).json({ error: "Not found" });
 });
