863b8255d046ccf920e991e3a16b74ec70f26173
fzl96 <fpsdragon333@gmail.com>
2024-09-03 12:20:42 +0700

    fix: add remote patterns for image
    fix and error when the remote pattern is not defined in config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,17 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.myanimelist.net",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
+};
 
 export default config;
